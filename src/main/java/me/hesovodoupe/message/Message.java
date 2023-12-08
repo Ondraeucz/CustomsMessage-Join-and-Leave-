13de167e4b05c54bc55bab25668bf9a5456c9a3c
Ondraeucz <98920258+Ondraeucz@users.noreply.github.com>
2023-12-08 20:32:11 +0100

    Add files via upload

--- src/main/java/me/hesovodoupe/message/Message.java
+++ src/main/java/me/hesovodoupe/message/Message.java
@@ -9,22 +9,23 @@ public final class Message extends JavaPlugin {
     public void onEnable() {
 
         System.out.println("Plugin has started!");
-        System.out.println("[WARN] There is a new configuration on version 1.3, to update remove the old config");
-        //start plugin
+        this.getLogger().warning("There is a new configuration on version 1.4, to update remove the old config");
+        //start plugin and information
 
         this.getLogger().info("---------------------------------------");
-        this.getLogger().info("CustomsMessage 1.3 version");
+        this.getLogger().info("CustomsMessage 1.4 version - ENABLE");
         this.getLogger().info("---------------------------------------");
         //annoucement console
 
         instance = this;
-        this.saveDefaultConfig();
+        saveDefaultConfig();
 
         this.getCommand("customsmessage").setExecutor(new reload());
         //commands
 
         getServer().getPluginManager().registerEvents(new JoinListener(this), this);
         getServer().getPluginManager().registerEvents(new LeaveListener(this), this);
+        getServer().getPluginManager().registerEvents(new UniqueListener(this), this);
         //register Join and Leave
 
         new UpdateCheck ( this , 113825 ) . getVersion ( version -> {
@@ -43,7 +44,7 @@ public final class Message extends JavaPlugin {
         //disable plugin
 
         this.getLogger().info("---------------------------------------");
-        this.getLogger().info("CustomsMessage 1.3 version");
+        this.getLogger().info("CustomsMessage 1.4 version - DISABLE");
         this.getLogger().info("---------------------------------------");
         //annoucement console {
     }
